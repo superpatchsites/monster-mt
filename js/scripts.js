2b813d1810d90859c4690195f3faafd46782b6c4
lingataira <airal@superpatch.com>
2024-08-02 16:16:10 -0400

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+function handleShare(platform) {
+	const shareData = {
+		title: 'Check out this contest!',
+		url: 'https://www.superpatch.com/',
+	};
+
+	if (navigator.share) {
+		navigator.share(shareData).catch((error) => console.error('Error sharing:', error));
+	} else {
+		navigator.clipboard.writeText(shareData.url)
+			.then(() => alert('Link copied to clipboard'))
+			.catch((error) => console.error('Error copying to clipboard:', error));
+	}
+}
+
+document.getElementById('shareButton').addEventListener('click', () => handleShare('default'));
+// document.getElementById('shareFacebook').addEventListener('click', () => handleShare('facebook'));
+// document.getElementById('shareTwitter').addEventListener('click', () => handleShare('twitter'));
+document.getElementById('shareInstagram').addEventListener('click', () => handleShare('instagram'));
+
+
